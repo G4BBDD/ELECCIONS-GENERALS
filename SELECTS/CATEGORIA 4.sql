5743ca56081b3dddf37bb6ced98c818454d1bda3
G4BBDD <124575785+G4BBDD@users.noreply.github.com>
2023-02-24 14:01:29 +0100

    Add files via upload

---
+++ SELECTS/CATEGORIA 4.sql
@@ -0,0 +1,16 @@
+/*Aquesta consulta recursiva ens torna tots els municipis que pertanyen a la mateixa comunitat autònoma que el municipi inicial que hem seleccionat*/
+WITH RECURSIVE municipis_comunitat AS (
+	SELECT m.municipi_id, m.nom, p.codi_ine, p.provincia_id, m.districte
+		FROM municipis m
+			INNER JOIN provincies p ON p.provincia_id = m.provincia_id
+				WHERE p.codi_ine = '08' /* Utilitzarem el cas de Catalunya*/
+				
+	UNION ALL
+    
+	SELECT m.municipi_id, m.nom, m.codi_ine, m.provincia_id, m.districte
+		FROM municipis m
+			INNER JOIN municipis_comunitat mc ON m.provincia_id = mc.provincia_id
+				WHERE m.codi_ine != '08' /* Excluimos los municipios que ya hemos incluido en la consulta */
+)
+SELECT *
+FROM municipis_comunitat;
