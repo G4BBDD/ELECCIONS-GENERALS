8f657a6220589f94a7cd14d4727c0df060b037ac
G4BBDD <124575785+G4BBDD@users.noreply.github.com>
2023-02-28 10:22:19 +0100

    Actualització del Script

--- BD_eleccions_v2.sql
+++ BD_eleccions_v2.sql
@@ -1,2 +1,13 @@
 ALTER TABLE comunitats_autonomes DROP INDEX uk_comunitats_autonomes_codi_ine;
 ALTER TABLE comunitats_autonomes ADD INDEX uk_comunitats_autonomes_codi_ine (codi_ine);
+
+ALTER TABLE municipis DROP INDEX uk_municipis_codi_ine;
+ALTER TABLE municipis ADD INDEX uk_municipis_codi_ine (codi_ine);
+
+ALTER TABLE persones MODIFY COLUMN data_naixement DATE NULL;
+
+ALTER TABLE persones DROP INDEX uk_persones_dni;
+ALTER TABLE persones ADD INDEX uk_persones_dni_nom (dni,nom);
+
+INSERT INTO eleccions (nom,data)
+	VALUES("Eleccions Generals 2019","2019-11-10")
