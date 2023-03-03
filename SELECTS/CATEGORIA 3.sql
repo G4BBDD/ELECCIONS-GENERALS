d6ec3a5683b38cff8fd5d399d12b5a384ace34f9
G4BBDD <124575785+G4BBDD@users.noreply.github.com>
2023-03-03 12:59:33 +0100

    Update CATEGORIA 3.sql

--- SELECTS/CATEGORIA 3.sql
+++ SELECTS/CATEGORIA 3.sql
@@ -40,3 +40,25 @@ SELECT nom_curt, COUNT(*) AS num_candidaturas
 		WHERE eleccio_id = (SELECT eleccio_id FROM eleccions WHERE any = 2019) 
 			GROUP BY nom_curt;
                             
+/*Seleccionar tots els municipis de la provincia de Barcelona*/
+SELECT nom AS municipis FROM municipis
+	WHERE provincia_id = (SELECT provincia_id FROM provincies
+			WHERE nom = 'Barcelona');
+
+/*Seleccionar a quina provincia pertany el candidat més jove*/
+SELECT nom AS provincia FROM provincies
+	WHERE provincia_id = (SELECT provincia_id FROM candidats
+							WHERE persona_id = (SELECT persona_id FROM persones
+													WHERE data_naixement = (SELECT data_naixement FROM persones
+														ORDER BY data_naixement ASC LIMIT 1)));
+
+/*Seleccionar la candidatura amb més vots a la comunitat autònoma de Madrid*/
+SELECT nom_curt AS candidatura FROM candidatures
+	WHERE candidatura_id = (SELECT candidatura_id FROM vots_candidatures_ca
+								WHERE comunitat_aut_id = 12
+									ORDER BY vots DESC LIMIT 1);
+
+/*Seleccionar tots els municipis de la Comunitat Valenciana que comencin amb la lletra D*/
+SELECT nom AS municipis FROM municipis
+	WHERE provincia_id IN (SELECT provincia_id FROM provincies
+							WHERE comunitat_aut_id = 17) AND nom LIKE 'D%';
