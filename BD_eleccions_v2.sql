fb7f483b8161b1cf0684624238b0dc3b3e06a7e3
G4BBDD <124575785+G4BBDD@users.noreply.github.com>
2023-03-03 12:18:01 +0100

    Update BD_eleccions_v2.sql

--- BD_eleccions_v2.sql
+++ BD_eleccions_v2.sql
@@ -5,9 +5,10 @@ ALTER TABLE municipis DROP INDEX uk_municipis_codi_ine;
 ALTER TABLE municipis ADD INDEX uk_municipis_codi_ine (codi_ine);
 
 ALTER TABLE persones MODIFY COLUMN data_naixement DATE NULL;
-
-ALTER TABLE persones DROP INDEX uk_persones_dni;
-ALTER TABLE persones ADD INDEX uk_persones_dni_nom (dni,nom);
+ALTER TABLE persones MODIFY COLUMN dni CHAR(9) NULL;
 
 INSERT INTO eleccions (nom,data)
-	VALUES("Eleccions Generals 2019","2019-11-10")
+	VALUES("Eleccions Generals 2019","2019-11-10");
+    
+ALTER TABLE vots_candidatures_ca
+	RENAME COLUMN comunitat_autonoma_id TO comunitat_aut_id;
