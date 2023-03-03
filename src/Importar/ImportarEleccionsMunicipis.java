8efd93f34d471cd295b6c21ee8b5068be41b43f3
G4BBDD <124575785+G4BBDD@users.noreply.github.com>
2023-03-03 03:35:28 +0100

    Codi utilitzat per importar eleccions_municipis

--- src/Importar/ImportarEleccionsMunicipis.java
+++ src/Importar/ImportarEleccionsMunicipis.java
@@ -12,8 +12,8 @@ import java.sql.SQLException;
 public class ImportarEleccionsMunicipis {
 
     public static void main() {
-        String dbURL = "jdbc:mysql://192.168.56.103:3306/eleccions_generals?useUnicode=true&characterEncoding=utf-8"; // URL de la BBDD real
-        //String dbURL = "jdbc:mysql://10.2.157.223:3306/eleccions_generals?useUnicode=true&characterEncoding=utf-8";
+        //String dbURL = "jdbc:mysql://192.168.56.103:3306/eleccions_generals?useUnicode=true&characterEncoding=utf-8"; // URL de la BBDD real
+        String dbURL = "jdbc:mysql://10.2.157.223:3306/eleccions_generals?useUnicode=true&characterEncoding=utf-8";
         String usuari = "perepi";
         String contrasenya = "pastanaga";
         String filePath = "C:\\Users\\34622\\Desktop\\02201911_MESA\\05021911.DAT";
