de636fecbb9ea1d8eed8e034be570e49514d9055
G4BBDD <124575785+G4BBDD@users.noreply.github.com>
2023-03-03 03:38:18 +0100

    Codi utiltzat per importar les Persones

--- src/Importar/ImportarPersones.java
+++ src/Importar/ImportarPersones.java
@@ -12,8 +12,8 @@ public class ImportarPersones {
     public static void main() {
         System.out.println("Importando Personas...");
         String filePath = "C:\\Users\\34622\\Desktop\\02201911_MESA\\04021911.DAT"; // Ruta del archivo .DAT
-        String dbURL = "jdbc:mysql://192.168.56.103:3306/eleccions_generals?useUnicode=true&characterEncoding=utf-8"; // URL de la BBDD real
-        //String dbURL = "jdbc:mysql://10.2.157.223:3306/eleccions_generals?useUnicode=true&characterEncoding=utf-8"; // URL de la BBDD
+        //String dbURL = "jdbc:mysql://192.168.56.103:3306/eleccions_generals?useUnicode=true&characterEncoding=utf-8"; // URL de la BBDD real
+        String dbURL = "jdbc:mysql://10.2.157.223:3306/eleccions_generals?useUnicode=true&characterEncoding=utf-8"; // URL de la BBDD
         String usuari = "perepi"; // Nombre de usuario de MySQL
         String contrasenya = "pastanaga"; // Contraseña de MySQL
 
