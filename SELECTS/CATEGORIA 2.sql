db6c190d8300b7ea6a07cefdce24e8e22faa3ee4
G4BBDD <124575785+G4BBDD@users.noreply.github.com>
2023-03-03 12:29:50 +0100

    Update CATEGORIA 2.sql

--- SELECTS/CATEGORIA 2.sql
+++ SELECTS/CATEGORIA 2.sql
@@ -1,11 +1,11 @@
-/*Mostrar la quantitat de candidats per cada municipi */
+/*Mostrar la quantitat de candidats per cada municipi i ordenals pels candidats de forma descendent */
 
 SELECT m.nom AS Municipi, m.municipi_id AS Municipi_id, COUNT(c.candidat_id) AS Candidats
 	FROM municipis m
 			INNER JOIN provincies p ON p.provincia_id = m.provincia_id
             		INNER JOIN candidats c ON c.provincia_id = p.provincia_id
-				GROUP BY m.municipi_id
-					ORDER BY m.municipi_id;
+						GROUP BY m.municipi_id
+							ORDER BY candidats DESC;
 
 
 
@@ -14,14 +14,14 @@ SELECT m.nom AS Municipi, m.municipi_id AS Municipi_id, COUNT(c.candidat_id) AS 
 SELECT em.vots_emesos AS "Vots emesos", em.vots_candidatures AS "Vots candidatures", em.vots_valids AS "Vots valids", em.vots_blanc AS 'Vots en blanc', em.vots_nuls AS "Vots nuls"
     FROM eleccions_municipis em
 		INNER JOIN eleccions e ON e.eleccio_id = em.eleccio_id
-			WHERE e.mes = 7; 
+			WHERE e.mes = 7;
             
 
 /* Mostra la quantitat de candidats per cada comunitat autònoma i la quantitat de vots */
 
 SELECT c.comunitat_aut_id AS Comunitat_Aut_Id, c.nom AS Comunitat_Autonoma, vo.vots AS Vots, COUNT(DISTINCT ca.candidat_id) AS Candidats
 	FROM comunitats_autonomes c
-		INNER JOIN vots_candidatures_ca vo ON vo.comunitat_autonoma_id = c.comunitat_aut_id
+		INNER JOIN vots_candidatures_ca vo ON vo.comunitat_aut_id = c.comunitat_aut_id
        		INNER JOIN provincies p ON p.comunitat_aut_id = c.comunitat_aut_id
         	INNER JOIN candidats ca ON ca.provincia_id = p.provincia_id	
 			GROUP BY c.comunitat_aut_id, c.nom, vo.vots
@@ -31,21 +31,21 @@ SELECT c.comunitat_aut_id AS Comunitat_Aut_Id, c.nom AS Comunitat_Autonoma, vo.v
 
 SELECT voca.vots AS Comunitat_Autonomes, vop.vots AS Provincies, vom.vots AS Municipis
 	FROM vots_candidatures_ca voca
-		INNER JOIN comunitats_autonomes c ON c.comunitat_aut_id = voca.comunitat_autonoma_id
-        	INNER JOIN provincies p ON p.comunitat_aut_id = c.comunitat_aut_id
+		INNER JOIN comunitats_autonomes c ON c.comunitat_aut_id = voca.comunitat_aut_id
+		INNER JOIN provincies p ON p.comunitat_aut_id = c.comunitat_aut_id
 		RIGHT JOIN vots_candidatures_prov vop ON vop.provincia_id = p.provincia_id
-        	INNER JOIN municipis m ON m.provincia_id = p.provincia_id
-        	RIGHT JOIN vots_candidatures_mun vom ON vom.municipi_id = m.municipi_id
+		INNER JOIN municipis m ON m.provincia_id = p.provincia_id
+		RIGHT JOIN vots_candidatures_mun vom ON vom.municipi_id = m.municipi_id
 			GROUP BY voca.vots, vop.vots, vom.vots;
 
-/* Mostra el nom, l'edat, el partit i la província dels candidats que tenen menys de 25 anys.*/
+/* Obtén el nom complet del candidat, el seu partit i la província a què es presenta en una elecció específica. */
 
-SELECT CONCAT(p.nom, ' ', p.cognom1, ' ', p.cognom2) AS nom_complet, TIMESTAMPDIFF(YEAR,p.data_naixement,CURDATE()) AS edat,  c.nom_curt AS partit,  pr.nom AS provincia
-	FROM candidats ca  
+SELECT CONCAT(p.nom, ' ', p.cognom1, ' ', p.cognom2) AS nom_complet,  cd.nom_curt AS partit, pr.nom AS provincia 
+	FROM candidats ca 
 		INNER JOIN persones p ON ca.persona_id = p.persona_id 
-		INNER JOIN candidatures c ON ca.candidatura_id = c.candidatura_id 
-		INNER JOIN provincies pr ON ca.provincia_id = pr.provincia_id 
-			WHERE timestampdiff(YEAR,data_naixement,CURDATE()) < 25;
+		INNER JOIN candidatures cd ON cd.candidatura_id = ca.candidatura_id 
+		INNER JOIN provincies AS pr ON ca.provincia_id = pr.provincia_id
+			WHERE cd.eleccio_id = 1;
 
 
 
