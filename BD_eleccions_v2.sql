2c5f5586cabd7ec3720b5fd4cb77b46cfc2463a3
G4BBDD <124575785+G4BBDD@users.noreply.github.com>
2023-03-03 13:41:42 +0100

    Modificacións al script inicial

--- BD_eleccions_v2.sql
+++ BD_eleccions_v2.sql
@@ -1,3 +1,5 @@
+*Modificacións al script inicial*
+
 ALTER TABLE comunitats_autonomes DROP INDEX uk_comunitats_autonomes_codi_ine;
 ALTER TABLE comunitats_autonomes ADD INDEX uk_comunitats_autonomes_codi_ine (codi_ine);
 
