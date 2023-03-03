56cabc40787ec0bc8dc0460f1ddf1d545b27ec06
G4BBDD <124575785+G4BBDD@users.noreply.github.com>
2023-03-03 03:43:18 +0100

    Codi utiltzat per importar els Vots Provincials

--- src/Importar/ImportarVotsProvincials.java
+++ src/Importar/ImportarVotsProvincials.java
@@ -15,8 +15,8 @@ public class ImportarVotsProvincials {
 
         System.out.println("Importando Vots Prov...");
         String filePath = "C:\\Users\\34622\\Desktop\\02201911_MESA\\08021911.DAT"; // Ruta del archivo .DAT
-        String dbURL = "jdbc:mysql://192.168.56.103:3306/eleccions_generals?useUnicode=true&characterEncoding=utf-8"; // URL de la BBDD real
-        //String dbURL = "jdbc:mysql://10.2.157.223:3306/eleccions_generals?useUnicode=true&characterEncoding=utf-8"; // URL de la BBDD
+        //String dbURL = "jdbc:mysql://192.168.56.103:3306/eleccions_generals?useUnicode=true&characterEncoding=utf-8"; // URL de la BBDD real
+        String dbURL = "jdbc:mysql://10.2.157.223:3306/eleccions_generals?useUnicode=true&characterEncoding=utf-8"; // URL de la BBDD
         String usuari = "perepi"; // Nombre de usuario de MySQL
         String contrasenya = "pastanaga"; // Contraseña de MySQL
 
